fac2173f947b06ab21ed551e5718d8fd6b4ecc2a
Rome <romanvela91@gmail.com>
2020-04-26 20:11:40 -0500

    Comments to method and instance variables

--- DogClass.py
+++ DogClass.py
@@ -5,9 +5,11 @@ class Dog:
         self.age = age
         self.aggressive = aggressive
 
+# returns dog info
     def dog_info(self):
         return f"Name: {self.name}\nBreed: {self.breed}\nAge: {self.age}\nAgressive: {self.aggressive}"
 
-
+# assinging attributes to class instances
 dog1 = Dog("Rocko", "German Shepherd", 7, False)
 dog2 = Dog("Ella", "Dachshund", 14, True)
+
